767f268ce292fbaaed45ba7682fcfed976b7ac4d
ozcanbayram <ozzcanbayram@gmail.com>
2024-03-18 11:48:27 +0300

    comment_model.dart

---
+++ learning_flutter/lib/service/comment_model.dart
@@ -0,0 +1,27 @@
+class CommentModel {
+  int? postId;
+  int? id;
+  String? name;
+  String? email;
+  String? body;
+
+  CommentModel({this.postId, this.id, this.name, this.email, this.body});
+
+  CommentModel.fromJson(Map<String, dynamic> json) {
+    postId = json['postId'];
+    id = json['id'];
+    name = json['name'];
+    email = json['email'];
+    body = json['body'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['postId'] = this.postId;
+    data['id'] = this.id;
+    data['name'] = this.name;
+    data['email'] = this.email;
+    data['body'] = this.body;
+    return data;
+  }
+}
--- learning_flutter/lib/service/post_service.dart
+++ learning_flutter/lib/service/post_service.dart
@@ -2,14 +2,18 @@ import 'dart:io';
 
 import 'package:dio/dio.dart';
 
+import 'comment_model.dart';
 import 'post_model.dart';
 
 abstract class IPostService {
   Future<bool> addItemToService(PostModel postModel);
   Future<bool> putItemToService(PostModel postModel, int id);
   Future<List<PostModel>?> fetchPostItemsAdvance();
+  Future<List<CommentModel>?> fetchRelatedCommentWithPostId(int postId);
   Future<bool> deleteItemToService(int id);
 }
+
+
 
 class PostService implements IPostService {
   final Dio _dio;
@@ -70,6 +74,27 @@ class PostService implements IPostService {
     }
     return null;
   }
+
+  @override
+  Future<List<CommentModel>?> fetchRelatedCommentWithPostId(int postId) async {
+    try {
+      final response = await Dio().get(_PostServicePaths.comments.name,
+          queryParameters: {_PostQueryPaths.postId.name: postId});
+
+      if (response.statusCode == HttpStatus.ok) {
+        final _datas = response.data;
+
+        if (_datas is List) {
+          return _datas.map((e) => CommentModel.fromJson(e)).toList();
+        }
+      }
+    } on DioException catch (error) {
+      print(error.message);
+    }
+    return null;
+  }
 }
 
 enum _PostServicePaths { posts, comments }
+
+enum _PostQueryPaths { postId, comments }
