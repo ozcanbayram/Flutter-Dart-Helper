aab6d0c8b45c6c84e9db356b0f2dcd02ab7d887c
ozcanbayram <ozzcanbayram@gmail.com>
2024-04-01 09:47:45 +0300

    image_learn_202.dart

--- learning_flutter/lib/main.dart
+++ learning_flutter/lib/main.dart
@@ -8,6 +8,7 @@ import 'package:flutter_full_learn_one/101/list_tile_learn.dart';
 //import 'package:flutter_full_learn_one/lesson_one/container_sized_box_learn.dart';
 import 'package:flutter_full_learn_one/101/scaffold_learn.dart';
 import 'package:flutter_full_learn_one/101/stack_learn.dart';
+import 'package:flutter_full_learn_one/202/image_learn_202.dart';
 import 'package:flutter_full_learn_one/202/theme/light_theme.dart';
 import 'package:flutter_full_learn_one/202/theme/theme_learn_view.dart';
 import 'package:flutter_full_learn_one/demos/note_demos_view.dart';
@@ -76,7 +77,7 @@ class MyApp extends StatelessWidget {
       //         centerTitle: true,
       //         backgroundColor: Colors.transparent,
       //         elevation: 0)),
-      home: ThemeLearnView(),
+      home: ImageLearn202(),
     );
   }
 }
