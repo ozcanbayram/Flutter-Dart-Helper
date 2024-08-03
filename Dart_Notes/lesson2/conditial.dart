f11f43e36e43a1cc6eb51b5da8fde568d6f09349
Ozcan Bayram <ozzcanbayram@gmail.com>
2024-08-03 22:57:32 +0300

    Basic if else example

---
+++ Dart_Notes/lesson2/conditial.dart
@@ -0,0 +1,14 @@
+void main() {
+  // Bazı if else örnekleri:
+  // Temel olarak if else kontroller için kullanılabilir. 
+  //İçerisinde veriler ile ilgili bir çok şeyi kontrol ederek durumlara göre işlemleri yönetebiliriz.
+  final int money = 100;
+
+  if (money < 100) {
+    print('Paran 100 den az.');
+  } else if (money == 100) {
+    print('paran 100\'e eşit');
+  } else {
+    print('paran 100\'den fazla.');
+  }
+}
