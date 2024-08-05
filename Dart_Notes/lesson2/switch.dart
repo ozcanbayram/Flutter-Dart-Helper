703a87751c1f45c65285f7459033a9972f5fe168
Ozcan Bayram <ozzcanbayram@gmail.com>
2024-08-05 21:59:41 +0300

    Switch case example.

---
+++ Dart_Notes/lesson2/switch.dart
@@ -0,0 +1,23 @@
+void main() {
+  //Örnek bir switch case yapısı:
+  final int degree = 3;
+  // Eger deger 0 ise çok kotu, 1 ise kotu, 2 ise nahoş, 3 ise ideal, 4 ise iyi, 5 ise cok iyi yazsın: dier durumlarda belirsiz yazsın.
+  switch (degree) {
+    case 0:
+      print('Çok Kötü');
+    case 1:
+      print('Kotu');
+    case 2:
+      print('nahoş');
+    case 3:
+      print('ideal');
+    case 4:
+      print('iyi');
+    case 5:
+      print('çok iyi');
+    default:
+      print('Belirsiz.');
+  }
+
+  //Switch ile kontrol edilecek değeri belirleriz, case ile kontrol edilecek ihtimaller için durumlar belirleriz. Default ile, caselerin hiçbiri sağlanmazsa ne olması gerektiğini belirleriz.
+}
