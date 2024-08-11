57337cc607ea9b8c3f025423814718782756b2b3
Ozcan Bayram <ozzcanbayram@gmail.com>
2024-08-11 22:42:06 +0300

    edited lists with comments.

--- Dart_Notes/lesson3/list.dart
+++ Dart_Notes/lesson3/list.dart
@@ -1,90 +1,55 @@
 void main() {
-  // Bir bankaya on musteri gelir bunlarin 100 tlsi 110tl 50 tlsi vardir
+  //Bir bankaya 10 musteri gelir 100, 110, 50 .. .tlsi vardır
 
-  final int money1 = 100;
-  final int money2 = 110;
+  List<int> moneys = [100, 200, 300, 500, 400];
+  final List<int> newMoney = [
+    10,
+    20,
+    30,
+    40,
+    50
+  ]; //final olduğu için değiştirilemez ama referans ile değer eklemek gibi özellikler yapılabilir.
 
-  // 100 buyuk olanlara hosgeldiniz beyfendi
+  print('1. Müşterinin parası: ${moneys[0]}');
 
-  if (money1 > 100) {
-    print('beyfendi');
-  }
-
-  if (money2 > 100) {
-    print('aa');
-  }
-
-  String name = '';
-  // 0, 1, 2
-  List<int> moneys = [100, 110, 500, 200, 300];
-  final List<int> newMoneys = [100, 110, 500, 200, 300];
-
-  print('musteri 1 parasi: ${moneys[0]}');
-
-  // paralari parasi buyuk olana gore
-
+  //parasi buyuk-kucuk olana gore:
   moneys.sort();
-  moneys.add(15);
-  moneys.insert(2, 300);
-  moneys.insert(0, 5);
+  //listeye ekleme yapma:
+  moneys.add(600);
+
+  moneys.insert(0, 50); //0. indexe ekler.
+
   print(moneys);
 
-  // newMoneys = [];
-  newMoneys.add(5);
-  newMoneys.clear();
+  newMoney.add(90); //referans ile final olan listenin içierisne erişebiliyoruz.
+  newMoney.clear();
 
-  print(newMoneys);
-  // newMoneys.reversed.toList();
+  newMoney.reversed
+      .toList(); // Listeyi tersine çevirir ve toList ile yeni bir liste oluşturur.
 
-  // 100 tanme musteri yap bunlarin hepsine sira ile numarasina gore 5 tl ekle
+  //Örnek:
+  //musteri listesi yap ve her musterinin numarasına göre sirayla 5 tl ekle.
 
   List<double> customerMoney = List.generate(100, (index) {
+    //generate ile lenght ve her indexe yapılacak işlemi belirtleriz.
     return index + 5;
   });
   print(customerMoney);
 
-  // Customers 100  30 40 60
-  // 35 tl den buyuk olanlari burada kredi vereiblirz yaz
-  // kucuk olanlar bye
+  print('-----------------------');
+  // Örnek:
+  // customers: 20,100,30,40,60
+  // 35 tlden buyuk olanlara kredi verebiliriz yazsın.
+  // kucuk olanlara baybay yazsın.
 
-  List<int> moneyCustomerNews = [100, 30, 40, 60, -5];
+  List<int> moneyCustomerNews = [100, 30, 20, 40, 60];
   moneyCustomerNews.sort();
-  for (int index = 0; index < moneyCustomerNews.length; index += 1) {
-    print('musteri parasi: ${moneyCustomerNews[index]}');
-    if (moneyCustomerNews[index] > 35) {
-      print('kredi hazir');
-    } else if (moneyCustomerNews[index] > 0) {
-      print('kredi veremeyiz ama bi bakalim');
+  for (int i = 0; i < moneyCustomerNews.length; i++) {
+    if (moneyCustomerNews[i] < 35) {
+      print('${moneyCustomerNews[i]}, paranız var, size kredi veremeyiz');
     } else {
-      print('by');
-    }
-  }
-  print('----');
-  for (int index = moneyCustomerNews.length - 1; index >= 0; index += -1) {
-    print('musteri parasi: ${moneyCustomerNews[index]}');
-    if (moneyCustomerNews[index] > 35) {
-      print('kredi hazir');
-    } else if (moneyCustomerNews[index] > 0) {
-      print('kredi veremeyiz ama bi bakalim');
-    } else {
-      print('by');
-    }
-  }
-
-  print('aaaa');
-  List<dynamic> users = [1, 'a', true];
-
-  for (var item in users) {
-    print(item);
-  }
-
-  List<String> userNames = ['ali', 'mehmet', 'ayse'];
-
-  userNames.contains('veli');
-
-  for (var item in userNames) {
-    if (item == 'veli') {
-      print('var');
+      print(
+          'oooo! ${moneyCustomerNews[i]}, paranız var size kredi verelim hemen');
     }
   }
 }
