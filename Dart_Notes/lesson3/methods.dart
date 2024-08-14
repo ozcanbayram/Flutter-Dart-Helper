d9a70762c62b117d7432fbf0a2c0696607bdb972
Ozcan Bayram <ozzcanbayram@gmail.com>
2024-08-14 15:26:54 +0300

    Updated method.dart

--- Dart_Notes/lesson3/methods.dart
+++ Dart_Notes/lesson3/methods.dart
@@ -1,66 +1,36 @@
-void main() {
-  // benim bir musterim var parasi var mi yok mu kontrol eder misin
+void main(List<String> args) {
+  int user1Money = 0;
 
-  final int userMoney = 0;
-  controlUserMoney(userMoney, 0);
-  // if (userMoney > 5) {
-  //   print('parasi var');
-  // } else {
-  //   print('parasi yok');
-  // }
+  //Kullanıcının parasının olup olmadığını tek tek kontrol etmek yerine controllUserMoney adında bir metot
+  // yazarak daha az kod ile daha temiz bir şekilde aynı işlemi yapabiliriz.
+  controllUserMoney(user1Money);
 
-  // benim bi usterim daha geldi onada bakar misin
-  final int user2Money = 5;
+  //musterinin parasini dolara çeviren bir metot yazalim:
+  int customerMoney = 26000;
+  int newCustomerMoney = convertToDolar(customerMoney);
+  print(newCustomerMoney);
 
-  controlUserMoney(user2Money, 5);
-  // if (user2Money > 5) {
-  //   print('parasi var');
-  // } else {
-  //   print('parasi yok');
-  // }
-
-  // ya dedi acaba 0 degilde en azindan bi 5tl  si olsun
-
-  //yeni musteri gelir parasi 50 tl
-  // parasini dolar yapmak istiyor ve kac dolar yaptgini merak ediyor
-
-  final newUserMoney = 50;
-
-  print(newUserMoney / 13);
-
-  int result = convertToDolar(newUserMoney);
-
-  print(result);
-  if (result > 0) {}
-
-  // bana bir dolar hesaplama yapp eger ben sana soyelmezsem yeni bir durum var diye her zaman 13 al
-
-  final newResult = convertToStandartDolar(100, dolarIndex: 13);
-  final newResult2 = convertToStandartDolar(100);
-  final newREsult3 = convertToEuro(userMoney: 500);
-  sayHello('aa');
+  int customer2Money = 36000;
+  // --> Burada opsionel olarak euro fiyatı belirlenebilir: Eğer belirlenmez ise metottaki default değeri alır.
+  int newCustomer2Money = convertToEuro(customer2Money);
+  print(newCustomer2Money);
 }
 
-void controlUserMoney(int money, int minumumValue) {
-  if (money > minumumValue) {
-    print('para var');
+void controllUserMoney(int money) {
+  if (money > 0) {
+    print('Parasi var');
   } else {
-    print('para yok');
+    print('Parasi yok');
   }
 }
 
-int convertToDolar(int userMoney) {
-  return userMoney ~/ 13;
+//musterinin parasini dolara çeviren bir metot yazalim:
+int convertToDolar(int money) {
+  return money ~/ 33;
 }
 
-int convertToStandartDolar(int userMoney, {int dolarIndex = 14}) {
-  return userMoney ~/ dolarIndex;
-}
-
-int convertToEuro({required int userMoney, int dolarIndex = 14}) {
-  return userMoney ~/ dolarIndex;
-}
-
-String sayHello(String name) {
-  return 'hello $name';
+//Asağidaki metot yine euro çevirme işllemini yapar fakat, opsiyonel olarak euro belirlenebilir.
+//Eğer belirlenmez ise atanan default değer verilir.
+int convertToEuro(int money, {int euro = 34}) {
+  return money ~/ euro;
 }
