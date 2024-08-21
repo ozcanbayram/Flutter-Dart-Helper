8ea08a28b9d69fe7d099b5290cf643d38e69700f
Ozcan Bayram <ozzcanbayram@gmail.com>
2024-08-21 23:28:53 +0300

    Some edits (extends)

--- Dart_Notes/lesson4/class_extends.dart
+++ Dart_Notes/lesson4/class_extends.dart
@@ -1,7 +1,13 @@
 void main() {
-  User normalUser = User('Ali', 250);
+  User normalUser = User('Sadık', 250);
+  final userBank = BankUser('Servet', 200, 123546);
+  final userSpecial = SpecialUser('Ferit', 350, 123456, 50);
 
   normalUser.SayMoneyWithCompanyName();
+  userBank.SayMoneyWithCompanyName();
+  userSpecial.SayMoneyWithCompanyName();
+
+  print('${userSpecial.name} bey, bonus eklenmiş paranız: ${userSpecial.CalculateMoney}');
 }
 
 // 1- tüm müşterilerin paralarını banka ismi ile görebilmesini istiyorum. Bu fonksiyonu tüm sınıflarda
@@ -16,7 +22,7 @@ abstract class IUser {
   IUser(this.name, this.money);
 
   void SayMoneyWithCompanyName() {
-    print('Ahmet- $money paranız var.');
+    print('$name - $money paranız var.');
     //Burada diğer sınıflardaki money bilgisine erişmek için. Bu sınf için bir özellik ve constructor
     //oluşturmamız gerekir.
     //Fakat, diğer sınıftaki bomey ve name bilgisini kullanabilmek için, : super ile o sınıftan gönderilmeilidir.
@@ -33,19 +39,42 @@ class User extends IUser {
             money); //Buradaki name ve money bilgilerini super ile gönderelim.
 }
 
-class BankUser {
-  final String name;
-  final int money;
+class BankUser extends IUser {
+  // final String name;
+  // final int money;
   final int bankingCode;
 
-  BankUser(this.name, this.money, this.bankingCode);
+  BankUser(String name, int money, this.bankingCode) : super(name, money);
+
+  void SpecialUserLogic() {
+    print(
+        money); //burada moneyi kullanacağız fakat super ile zaten money'i IUser sınıfından aldığımız için.
+    // this.name, this.money, this.bankingCode satırını  String name, int money, this.bankingCode olarak
+    // degistirebilir ve bu sınıf içerisindeli final String name ve final int money tanımlamalarını silebiliriz.
+    // Boylelikle her yerde name ve money tanımlamak yerine abstract classta tanımlayarak gruplamış oluruz.
+  }
 }
 
-class SpecialUser {
+class SpecialUser extends IUser {
   final String name;
   final int money;
   final int bankingCode;
-  final int disccount;
+  late final int _disccount;
+  //sadece buradan erişilmesi için: _ kullanarak private yaptık.
 
-  SpecialUser(this.name, this.money, this.bankingCode, this.disccount);
+  SpecialUser(this.name, this.money, this.bankingCode, int disccount)
+      : super(name, money) {
+    _disccount = disccount;
+    //Bu işlem ile private olan _disccount'u disccounta eşitleyerek kullanılabilir hale getirdik.
+  }
+
+  //indirimli fiyat hesaplama:
+
+  // int CalculateMoney() {
+  //   return money - (money ~/ disccount);
+  // }
+
+  //Get ile aynı işlemi yapalım:
+
+  int get CalculateMoney => money + (money ~/ _disccount);
 }
