767885310355ace16a8e8143084a4ab73e58625e
Ozcan Bayram <ozzcanbayram@gmail.com>
2024-08-26 01:08:05 +0300

    Basic algorithm examples.

---
+++ Dart_Notes/algorithm_questions/odd_or_even.dart
@@ -0,0 +1,23 @@
+//! Bu örnekte bir sayının tek mi çift mi olduğunu bulalım:
+//* eğer sayi çift ise true, tek ise false yazdıralım.
+void main() {
+  final int sayi1 = 150; //? Sayıyı belirleyelim
+  final int sayi2 = 155; //? Sayıyı belirleyelim
+
+//! Fonksitonu sayı için uygulayarak tek/çift kontrolü yapalım ve sonucu print ile yazdıralım.
+  print(isOddOrEven(sayi1));
+  //! Fonksitonu sayı için uygulayarak tek/çift kontrolü yapalım ve sonucu print ile yazdıralım.
+  print(isOddOrEven(sayi2));
+}
+
+//* Tek çift kontrolünü yapan fonksiyon:
+//? Fonksiyonu tanımlayalım ve bool değişleninde bir değer döndüreceğini belirtelim (bool = True - False). Parametre olarak bir int değer verelim (int x)
+bool isOddOrEven(int x) {
+  if (x % 2 == 0) {
+    //? Eğer sayının 2 ile bölümünden kalan 0 ise sayı çifttir. True dönmesini belirleriz. (Örneğin 10/2 kalan = 0)
+    return true; //* return ile değer döndürürüz.
+  } else {
+    //? Eğer sayının 2 ile bölümünden kalan 0 değilse false döndürmeyi belirleriz.
+    return false; //* return ile değer döndürürüz.
+  }
+}
