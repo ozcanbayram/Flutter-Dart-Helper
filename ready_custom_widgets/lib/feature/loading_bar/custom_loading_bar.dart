f7df980f0bfd086c87395e7e386ba069fe280a4c
Ozcan Bayram <ozzcanbayram@gmail.com>
2024-09-01 17:40:28 +0300

    New loadding bars

--- ready_custom_widgets/lib/feature/loading_bar/custom_loading_bar.dart
+++ ready_custom_widgets/lib/feature/loading_bar/custom_loading_bar.dart
@@ -15,6 +15,7 @@ class tLoadingBarState extends State<LoadingBar> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        toolbarHeight: 80,
         backgroundColor: Colors.red,
         title: const Center(
           child: Text(
@@ -23,28 +24,109 @@ class tLoadingBarState extends State<LoadingBar> {
           ),
         ),
       ),
-      body: const Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SizedBox(
-            width: 400,
-            height: 200,
-            child: Card(
-              child: Column(
-                children: [
-                  SpinKitDancingSquare(
-                    size: 80,
-                    color: Colors.red,
-                  ),
-                  Text(
-                    'Dancing Square',
-                    style: TextStyle(fontSize: 35),
-                  ),
-                ],
+      body: const SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: 16),
+            Center(
+              child: Text(
+                'Bu sayfada flutter_spinkit paketi kullanıldı.',
+                style: TextStyle(
+                  fontSize: 25,
+                ),
+                textAlign: TextAlign.center,
               ),
             ),
+            SizedBox(height: 16),
+            _CustomSizedBox(
+              boxTitle: 'Dancing Square',
+              boxLoadingBar: SpinKitDancingSquare(
+                size: 80,
+                color: Colors.red,
+              ),
+            ),
+            _CustomSizedBox(
+              boxTitle: 'Rotating Plain',
+              boxLoadingBar: SpinKitRotatingPlain(
+                size: 80,
+                color: Colors.amber,
+              ),
+            ),
+            _CustomSizedBox(
+              boxTitle: 'Double Bounce',
+              boxLoadingBar: SpinKitDoubleBounce(
+                size: 80,
+                color: Colors.green,
+              ),
+            ),
+            _CustomSizedBox(
+              boxTitle: 'Wave',
+              boxLoadingBar: SpinKitWave(
+                size: 80,
+                color: Colors.lightBlueAccent,
+              ),
+            ),
+            _CustomSizedBox(
+                boxTitle: 'Fading Four',
+                boxLoadingBar:
+                    SpinKitFadingFour(size: 80, color: Colors.black)),
+            _CustomSizedBox(
+                boxTitle: 'Fading Cube',
+                boxLoadingBar: SpinKitFadingCube(
+                  size: 80,
+                  color: Colors.lightGreenAccent,
+                )),
+            _CustomSizedBox(
+                boxTitle: 'Pulsing Grid',
+                boxLoadingBar: SpinKitPulsingGrid(
+                  size: 80,
+                  color: Colors.orangeAccent,
+                )),
+            _CustomSizedBox(
+                boxTitle: 'Square Circle',
+                boxLoadingBar: SpinKitSquareCircle(
+                  size: 80,
+                  color: Colors.yellowAccent,
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CustomSizedBox extends StatelessWidget {
+  final String boxTitle;
+  final Widget boxLoadingBar;
+  const _CustomSizedBox({
+    // ignore: unused_element
+    super.key,
+    required this.boxTitle,
+    required this.boxLoadingBar,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0),
+      child: SizedBox(
+        // width: 400,
+        height: 180,
+        child: Card(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: boxLoadingBar,
+              ),
+              Text(
+                boxTitle,
+                style: const TextStyle(fontSize: 35),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
