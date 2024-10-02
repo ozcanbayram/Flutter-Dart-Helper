43af731105e3da7e9d47411319a1b2d418dec81e
Ozcan Bayram <ozzcanbayram@gmail.com>
2024-10-02 15:38:40 +0300

    Flutter Notes project is created and added in Flutter-Learning_Projects folder. Start improvements...

--- Dart_Notes/lesson6/solid.dart
+++ Dart_Notes/lesson6/solid.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-//* SOLID kavrami? Single Responsibility Principle (SRP)
+//? SOLID kavrami? 
+//* Single Responsibility Principle (SRP)
 void main(List<String> args) {
   IDatabase database = SQL();
   //Burada veriler SQL'den gelir fakat istediğimiz  zaman aşağıdaki gibi kolayca değiştirebiliriz.
