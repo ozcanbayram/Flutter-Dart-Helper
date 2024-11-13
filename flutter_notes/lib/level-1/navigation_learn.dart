ef0d44b3cc45891f3cf82bf994c0c68d3e2a96d3
Ozcan Bayram <ozzcanbayram@gmail.com>
2024-11-13 21:04:36 +0300

    Navigate to push

--- flutter_notes/lib/level-1/navigation_learn.dart
+++ flutter_notes/lib/level-1/navigation_learn.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notes/demos/my_collection_demo.dart';
 
 class NavigationLearn extends StatefulWidget {
   const NavigationLearn({super.key});
@@ -7,9 +8,35 @@ class NavigationLearn extends StatefulWidget {
   State<NavigationLearn> createState() => _NavigationLearnState();
 }
 
-class _NavigationLearnState extends State<NavigationLearn> {
+class _NavigationLearnState extends State<NavigationLearn>
+    with NavigatorManager {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Navigator'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          navigateToWidgetPush(context, const MyCollectionDemo());
+        },
+        child: const Icon(Icons.navigation_rounded),
+      ),
+    );
+  }
+}
+
+//! navigator mixin:
+mixin NavigatorManager {
+  void navigateToWidgetPush(BuildContext context, Widget widget) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) {
+          return widget;
+        },
+        fullscreenDialog: true, //* geri butonu yerine çarpı butonu
+        settings: const RouteSettings(),
+      ),
+    );
   }
 }
