73bb491d58b014fa8aef0f539f144d8bf99e403d
Ozcan Bayram <ozzcanbayram@gmail.com>
2024-11-16 18:07:33 +0300

    Basics BottomNav Bar and tabBar

--- flutter_notes/lib/level-2/tab_learn.dart
+++ flutter_notes/lib/level-2/tab_learn.dart
@@ -11,19 +11,56 @@ class _TabLearnState extends State<TabLearn> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text('Tab Learn'),
-          ),
-          body: TabBarView(children: [
-            Container(
-              color: Colors.red,
-            ),
-            Container(
-              color: Colors.green,
-            ),
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Tab Learn'),
+          //ust nav bar
+          bottom: const TabBar(tabs: [
+            Tab(text: 'page 1'),
+            Tab(text: 'page 2'),
           ]),
-        ));
+        ),
+        body: TabBarView(children: [
+          //! tab barlar ile gecilecek sayfa veya widgetları burada ayarlarız
+          Container(
+            color: Colors.red,
+          ),
+          Container(
+            color: Colors.green,
+          ),
+        ]),
+        //
+        extendBody: true, // floataction button ile bottomnavbar arasını açar.
+
+        //alt navbar
+        bottomNavigationBar: const BottomAppBar(
+          notchMargin: 10,
+          shape: CircularNotchedRectangle(),
+          child: TabBar(
+              //* diğer bazı özellikler:
+              // indicatorColor: ,
+              // isScrollable: true,
+              // labelColor: ,
+              // unselectedLabelColor: ,
+              // padding: ,
+              // onTap: (value) {},
+              
+              // controller: _tabController, şeklinde controller atanarak başka işlemler de yapılabilir.
+              //örneğin floataction Buttona basınca controller kullnarak bir sayfaya geri dönmek gibi.
+              tabs: [
+                Tab(text: 'page 1'),
+                Tab(text: 'page 2'),
+              ]),
+        ),
+
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Colors.blueAccent,
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButtonAnimator: FloatingActionButtonAnimator.scaling,
+      ),
+    );
   }
 }
