d03ec6ea04311211de4ad4764165103489d9deeb
Ozcan Bayram <ozzcanbayram@gmail.com>
2024-12-20 00:26:11 +0300

    Added new advence sheet widget. It's not finished yet.

--- flutter_notes/lib/level-2/sheet_learn.dart
+++ flutter_notes/lib/level-2/sheet_learn.dart
@@ -4,71 +4,88 @@ class SheetLearn extends StatefulWidget {
   const SheetLearn({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
   _SheetLearnState createState() => _SheetLearnState();
 }
 
-Color backgroundColor = Colors.white;
-
 class _SheetLearnState extends State<SheetLearn> {
+  Color _backgroundColor = Colors.white;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Sheet Learn'),
       ),
+      backgroundColor: _backgroundColor,
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.open_in_browser_rounded),
-        onPressed: () {
-          showModalBottomSheet(
-            backgroundColor: Colors.lightGreenAccent,
+        onPressed: () async {
+          final result = await showModalBottomSheet(
+            backgroundColor: Colors.white,
             isScrollControlled: true,
             barrierColor: const Color.fromARGB(167, 0, 0, 0),
             shape: const RoundedRectangleBorder(
                 borderRadius: BorderRadius.vertical(top: Radius.circular(32))),
             context: context,
             builder: (context) {
-              return CustomSheet(
-                backgroundColor: backgroundColor,
-              );
+              return _CustomSheet(backgroundColor: _backgroundColor);
             },
           );
+          if (result is bool) {
+            setState(() {
+              _backgroundColor = Colors.deepOrangeAccent;
+            });
+          }
         },
       ),
     );
   }
 }
 
-class CustomSheet extends StatelessWidget {
-  CustomSheet({super.key, required this.backgroundColor});
-  Color backgroundColor;
+class _CustomSheet extends StatefulWidget {
+  const _CustomSheet({
+    super.key,
+    required Color backgroundColor,
+  });
+
+  @override
+  State<_CustomSheet> createState() => _CustomSheetState();
+}
+
+class _CustomSheetState extends State<_CustomSheet> {
+  Color _backgroundColor = Colors.blueAccent;
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        SizedBox(
-          width: double.infinity,
-          child: Padding(
-            padding: const EdgeInsets.all(32.0),
-            child: Column(
-              children: [
-                const Text('Bottom Sheet'),
-                Image.network(
-                  'https://picsum.photos/200/300',
-                  height: 250,
-                ),
-                ElevatedButton(
-                    onPressed: () {
-                      setState() {
-                        backgroundColor = Colors.amber;
-                      }
-                    },
-                    child: const Text('Change Color'))
-              ],
+    return Container(
+      color: _backgroundColor,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          SizedBox(
+            width: double.infinity,
+            child: Padding(
+              padding: const EdgeInsets.all(32.0),
+              child: Column(
+                children: [
+                  const Text('Bottom Sheet'),
+                  Image.network(
+                    'https://picsum.photos/200/300',
+                    height: 250,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          _backgroundColor = Colors.pinkAccent;
+                        });
+                        Navigator.of(context).pop<bool>(true);
+                      },
+                      child: const Text('Change Color'))
+                ],
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
