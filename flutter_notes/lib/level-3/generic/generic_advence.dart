8ba8da1b09919342b2fac52ed7a3f9459958e5cc
Ozcan Bayram <ozzcanbayram@gmail.com>
2025-01-02 19:09:18 +0300

    Generic Advence

--- flutter_notes/lib/level-3/generic/generic_advence.dart
+++ flutter_notes/lib/level-3/generic/generic_advence.dart
@@ -1,32 +1,66 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-class GenreicUser {
-  String name;
-  String id;
-  int money;
-  GenreicUser({
-    required this.name,
-    required this.id,
-    required this.money,
-  });
-}
+import 'package:equatable/equatable.dart';
 
-class UserManagement {
-  void static(GenreicUser user) {
+class UserManagement<T extends AdminUser> {
+  final T admin;
+
+  UserManagement(this.admin);
+  void sayName(GenericUser user) {
     print(user.name);
   }
 
-  int calculateMoney(List<GenreicUser> users) {
+  int calculateMoney(List<GenericUser> users) {
+    // ignore: unused_local_variable
     int sum = 0;
-    for (var element in users) {
-      sum += element.money;
+    for (var item in users) {
+      sum += item.money;
     }
+    int initialValue = admin.role == 1 ? admin.money : 0;
 
-    final sumMoney = users
-        .map((e) => e.money)
-        .fold<int>(0, (previousValue, element) => previousValue + element);
+    // ignore: unused_local_variable
+    final sumMoney = users.map((e) => e.money).fold<int>(
+        initialValue, (previousValue, element) => previousValue + element);
 
-    final money = users.fold(
-        0, (previousValue, element) => previousValue + element.money);
-    return money;
+    final monney = users.fold<int>(initialValue,
+        (previousValue, element) => previousValue + element.money);
+    return monney;
   }
+
+  Iterable<OBModel<R>>? showNames<R>(List<GenericUser> users) {
+    if (R == String) {
+      final names =
+          users.map((e) => OBModel<R>(e.name.split('').toList().cast<R>()));
+      return names;
+    }
+    return null;
+  }
+}
+
+class OBModel<T> {
+  final String name = 'vb';
+  final List<T> items;
+
+  OBModel(this.items);
+}
+
+class GenericUser extends Equatable {
+  final String name;
+  final String id;
+  final int money;
+
+  const GenericUser(this.name, this.id, this.money);
+
+  bool findUserName(String name) {
+    return this.name == name;
+  }
+
+  @override
+  String toString() => 'GenericUser(name: $name, id: $id, money: $money)';
+
+  @override
+  List<Object?> get props => [id];
+}
+
+class AdminUser extends GenericUser {
+  final int role;
+  const AdminUser(super.name, super.id, super.money, this.role);
 }
--- flutter_notes/test/user_manager_test.dart
+++ flutter_notes/test/user_manager_test.dart
@@ -2,13 +2,18 @@ import 'package:flutter_notes/level-3/generic/generic_advence.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  setUp(
-    () {},
-  );
+  setUp(() {});
+  test('User calculate ', () {
+    final users = [
+      const GenericUser('ozcan', '13', 1),
+      const GenericUser('can', '15', 2),
+    ];
+    final userManagement =
+        UserManagement(const AdminUser('vadmin', '1', 15, 1));
+    final result = userManagement.calculateMoney(users);
 
-  test('Users Money Calculate Test', () {
-    final users = [GenreicUser(name: 'ozcan', id: '123', money: 50000)];
-    final result = UserManagement().calculateMoney(users);
-    expect(result, 40);
+    final response = userManagement.showNames<String>(users);
+
+    expect(result, 55);
   });
 }
