dda7799cf1dabbbd00680fde13b38ca61a9b87e7
Ozcan Bayram <ozzcanbayram@gmail.com>
2025-01-16 20:41:22 +0300

    Advence lottie with theme provider

--- flutter_notes/lib/level-3/lottie_animation.dart
+++ flutter_notes/lib/level-3/lottie_animation.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notes/product/constants/lottie_items.dart';
+import 'package:flutter_notes/product/global/theme_notifier.dart';
 import 'package:lottie/lottie.dart';
+import 'package:provider/provider.dart';
 
 class LottieLearn extends StatefulWidget {
   const LottieLearn({super.key});
@@ -9,14 +11,38 @@ class LottieLearn extends StatefulWidget {
   State<LottieLearn> createState() => _LottieLearnState();
 }
 
-// https://assets3.lottiefiles.com/packages/lf20_ydo1amjs.json
+class _LottieLearnState extends State<LottieLearn>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  bool isLight = false;
 
-class _LottieLearnState extends State<LottieLearn> {
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        actions: [Lottie.asset(LottieItems.themeChange.lottiePath)],
+        actions: [
+          InkWell(
+            child: Lottie.asset(
+              LottieItems.themeChange.lottiePath,
+              repeat: false,
+              controller: controller,
+            ),
+            onTap: () {
+              context.read<ThemeNotifier>().changeTheme();
+              controller.animateTo(isLight ? 0 : 1);
+              isLight = !isLight;
+            },
+          ),
+        ],
       ),
       body: Center(
         child: Lottie.network(
