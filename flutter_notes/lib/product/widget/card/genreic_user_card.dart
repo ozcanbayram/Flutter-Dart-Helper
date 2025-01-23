1ad8891e07ebe22d8ae0740fdc0672c0790ff234
Ozcan Bayram <ozzcanbayram@gmail.com>
2025-01-23 20:26:16 +0300

    Started a basic test example.

--- flutter_notes/lib/product/widget/card/genreic_user_card.dart
+++ flutter_notes/lib/product/widget/card/genreic_user_card.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notes/level-3/generic/generic_advence.dart';
 
 class GenericUserCard extends StatelessWidget {
-  
-  const GenericUserCard({super.key});
+  const GenericUserCard({super.key, required this.model});
+  final GenericUser model;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Text(model.name);
   }
 }
--- flutter_notes/test/list_test.dart
+++ flutter_notes/test/list_test.dart
@@ -1,16 +1,21 @@
 import 'package:flutter_notes/level-3/generic/generic_advence.dart';
+import 'package:flutter_notes/product/widget/card/high_card.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   setUp(() {});
-  test(
-    'Sample Test',
-    () {
-      final users = [
-        const GenericUser('Ozcan', '13', 1),
-        const GenericUser('Can', '15', 2),
-        const GenericUser('Ali', '27', 3),
-      ];
-    },
-  );
+  test('Sample Test', () {
+    final users = [
+      const GenericUser('Ozcan', '13', 1),
+      const GenericUser('Can', '15', 2),
+      const GenericUser('Ali', '27', 3),
+    ];
+
+    // ignore: unused_local_variable
+    List<HighCard> highCard = users.map(
+      (e) {
+        return HighCard(items: e.name.split('').toList());
+      },
+    ).toList();
+  });
 }
